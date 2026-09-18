5ad4133087a84936b1da50ce7dd0ba1700190f7a
Leticia Mantovani <leticiamantovani159@gmail.com>
2026-09-18 09:33:31 -0300

    fix: fix path

--- app/services/external_api_service.py
+++ app/services/external_api_service.py
@@ -1,5 +1,5 @@
 import httpx
-from core.config import external_api_url
+from app.core.config import external_api_url
 
 async def get_external_api_data(foods: list[str]) -> dict:
     response = httpx.get(external_api_url, params={"foods": foods})
